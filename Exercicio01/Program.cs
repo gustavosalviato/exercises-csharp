7a2b1a703004a56d2b0f226d9e32975d74fadc0e
gustavosalviato <gustavosalviato8@gmail.com>
2022-03-02 20:37:40 -0300

    Initial Commit

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um valor em R$: ");
+            string value = Console.ReadLine();
+
+            decimal convertValue = 0.0m;
+
+            decimal valorCambio = 0.1932m;
+
+            decimal.TryParse(value, out convertValue);
+
+            decimal valorCambioConvertido = convertValue * valorCambio;
+
+            Console.WriteLine($"Valor em $ : {valorCambioConvertido}");
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Exercicio02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um valor em USD:");
+            String valorDigitado = Console.ReadLine();
+
+            decimal valorConvertido = 0.00m;
+
+            decimal valorCambio = 5.22m;
+
+            decimal.TryParse(valorDigitado, out valorConvertido);
+
+            decimal valorCambioConvertido = valorConvertido / valorCambio;
+
+            Console.WriteLine($"Valor R$: {valorCambioConvertido}");
+
+        }
+    }
+}
